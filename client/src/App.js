cfa97a416eb29e681711f7b6dc0f8f86449aa896
danhamraj <danhamraj@gmail.com>
2019-12-18 22:07:24 -0500

    updated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,12 @@
 import React, { useState } from "react";
-
 import { Route } from "react-router-dom";
-
 import SavedList from "./Movies/SavedList";
-
 import MovieList from "./Movies/MovieList";
-
 import Movie from "./Movies/Movie";
-
 import UpdateMovie from "./Movies/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
-
   const [movieToEdit, setMovieToEdit] = useState({});
 
   const addToSavedList = movie => {
@@ -28,9 +22,7 @@ const App = () => {
   return (
     <>
       <SavedList list={savedList} />
-
       <Route exact path="/" component={MovieList} />
-
       <Route
         path="/movies/:id"
         render={props => {
